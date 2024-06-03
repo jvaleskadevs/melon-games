ca8223e8bc8c4eefcd49e7263911035f6bc23aef
jvaleskadevs <vmjune2019@gmail.com>
2024-06-03 15:31:44 +0200

    newGameTypeAddedQA

---
+++ app/qa/page.tsx
@@ -0,0 +1,49 @@
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import type { Metadata, ResolvingMetadata } from 'next';
+import { URL } from '../config';
+
+type Props = {
+  params: { img: string },
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const imageIndex: string = typeof searchParams.img === "string" 
+    ?  searchParams.img : '0';
+  
+  const title = 'Melon Games';
+  const description = 'Melon Games';
+  const image = `${URL}/qa.jpg`;
+
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      {
+        label: 'Play'
+      }
+    ],
+    image: { 
+      src: image, 
+      aspectRatio: '1.91:1' 
+    },
+    //input: { text: 'Some text' },
+    postUrl: `${URL}/api/frame/qa?img=${imageIndex}`
+  });
+  
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [image]
+    },
+    other: { ...frameMetadata }
+  }
+}
+
+export default function Page({ params, searchParams }: Props) {
+  return <><h1>Melon Games is a Farcaster frame!</h1></>
+}
