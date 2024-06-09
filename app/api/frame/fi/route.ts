5f7183421b6de7ed35f26d4f45c6e0912f5dafa1
jvaleskadevs <vmjune2019@gmail.com>
2024-06-09 21:03:35 +0200

    firstIMpressionNewGame

---
+++ app/api/frame/fi/route.ts
@@ -0,0 +1,56 @@
+import { getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+import {
+  init,
+  validateFramesMessage,
+  ValidateFramesMessageInput,
+  ValidateFramesMessageOutput,
+} from '@airstack/frames';
+import { toHex } from 'viem';
+import { URL } from '../../../config';
+import { Errors } from '../../../errors';
+
+init(process.env.AIRSTACK_API_KEY ?? '');
+
+async function getResponse(req: NextRequest): Promise<NextResponse> { 
+  try {
+    const body: ValidateFramesMessageInput = await req.json();
+    const { isValid, message } = await validateFramesMessage(body);
+    if (!isValid) return new NextResponse(Errors.NoValidMessage);
+  } catch (err) {
+    console.log(err);
+  }
+  
+  //const fid: number | undefined = message?.data?.fid || undefined;
+  //const action = message?.data?.frameActionBody || undefined;
+  
+  //console.log(toHex(action?.castId?.hash ?? ''));
+  //const text = action?.inputText?.[0] || '';
+  
+  /*
+  if (action?.buttonIndex === 1) {
+    console.log(fid);
+  }
+  */
+  
+  const imageIndex: string = req.nextUrl.searchParams.get('img') ?? '0';
+ 
+  return new NextResponse(getFrameHtmlResponse({
+    buttons: [
+      {
+        label: 'Play'
+      }
+    ],
+    image: {
+      src: `${URL}/fi_${imageIndex}.png`,
+      aspectRatio: '1:1'
+    },
+    postUrl: `${URL}/api/frame/fi?img=${imageIndex}`
+  }));
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
